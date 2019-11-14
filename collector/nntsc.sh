79b6f34940317150cb456558db6fba82d12e735c
Daniel Oosterwijk <danieloosterwijk@gmail.com>
2019-11-14 14:12:46 +1300

    Extra config and some image reliability

--- collector/nntsc.sh
+++ collector/nntsc.sh
@@ -2,6 +2,6 @@
 
 # Wait around for dependent services to start...
 # We could do this actively, but this should be long enough.
-sleep 5
+sleep 10
 
 exec /sbin/setuser cuz nntsc -C '/etc/nntsc/nntsc.conf'
